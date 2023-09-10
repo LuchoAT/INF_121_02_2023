c2148585e80d9cea98a29474b941a0fd3a6ea55a
Luis David Ajhuacho Tarquino <36093403+LuchoAT@users.noreply.github.com>
2023-09-10 11:43:14 -0400

    Add files via upload

---
+++ Clase_1/Vector.java
@@ -0,0 +1,9 @@
+package Clase_1;
+
+public class Vector {
+	// el tamanio del vector
+	protected int max = 50;
+	// el vector tendra capacidad de hasta 50 elementos
+	protected int v[] = new int[max];
+
+}
