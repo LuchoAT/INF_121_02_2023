0aac7b9b898d9b95c93b9d4f85b2788e5349f479
Luis David Ajhuacho Tarquino <36093403+LuchoAT@users.noreply.github.com>
2023-09-10 11:48:39 -0400

    Add files via upload

---
+++ Clase_2/src/Clase_2_ej1/Vector.java
@@ -0,0 +1,9 @@
+package Clase_2_ej1;
+
+public class Vector {
+	// el tamanio del vector
+	protected int max = 50;
+	// el vector tendra capacidad de hasta 50 elementos
+	protected Practica v[] = new Practica[max];
+
+}
---
+++ Clase_2/src/Clase_2_ej3/Main.java
@@ -0,0 +1,94 @@
+package Clase_2_ej3;
+
+public class Main {
+	// a) Verificar si esta ordenado
+	static boolean verif(PilaC A) {
+		PilaC auxPila = new PilaC();
+		int verif = 1;
+		double aux = -10000000;
+		while (!A.esVacia()) {
+			Caja x = A.eliminar();
+			if (aux > x.getArea()) {
+				verif = 0;
+			}
+			aux = x.getArea();
+			auxPila.adicionar(x);
+		}
+		A.vaciar(auxPila);
+		if (verif == 1) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	// b) Ordenar descent. respecto al area
+	static void ordenar(PilaC A) {
+		PilaC auxPila = new PilaC();
+		PilaC ord = new PilaC();
+		while (!A.esVacia()) {
+			double minValor = 10000000;
+			while (!A.esVacia()) {
+				Caja x = A.eliminar();
+				if (x.getArea() < minValor) {
+					minValor = x.getArea();
+				}
+				auxPila.adicionar(x);
+			}
+			A.vaciar(auxPila);
+			while (!A.esVacia()) {
+				Caja x = A.eliminar();
+				if (x.getArea() == minValor) {
+					ord.adicionar(x);
+				} else {
+					auxPila.adicionar(x);
+				}
+			}
+			A.vaciar(auxPila);
+		}
+		A.vaciar(ord);
+	}
+
+	// c) Agrupar por color
+	static void Agrupar(PilaC A) {
+		PilaC auxPila = new PilaC();
+		PilaC ans = new PilaC();
+		while (!A.esVacia()) {
+			Caja x = A.eliminar();
+			while (!A.esVacia()) {
+				Caja y = A.eliminar();
+				if (x.getColor().equals(y.getColor())) {
+					ans.adicionar(y);
+				} else {
+					auxPila.adicionar(y);
+				}
+			}
+			ans.adicionar(x);
+			A.vaciar(auxPila);
+		}
+		A.vaciar(ans);
+	}
+
+	public static void main(String[] args) {
+
+		PilaC A = new PilaC();
+		int n = Leer.datoInt();
+		A.llenar(n);
+		A.mostrar();
+		//Inciso a)
+		/*if(verif(A)) {
+			System.out.println("La pila esta ordenada desc.");
+		}else {
+			System.out.println("La pila NO esta ordenada desc.");
+		}*/
+		
+		//Inciso b)
+		ordenar(A);
+		A.mostrar();
+		//Inciso c)
+		Agrupar(A);
+		A.mostrar();
+	
+	}
+
+}
